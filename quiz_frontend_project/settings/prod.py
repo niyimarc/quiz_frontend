ab779d2efebf26cc0bbc901705d67aa6941ef230
Drey <oluwadamilareadeniye@yahoo.com>
2025-09-06 11:16:39 +0100

    setup allowed hosts and static url

--- quiz_frontend_project/settings/prod.py
+++ quiz_frontend_project/settings/prod.py
@@ -1,3 +1,14 @@
 from .base import *
 
 DEBUG = False
+ALLOWED_HOSTS = ["www.quiz.speedspro.us", "quiz.speedspro.us"]
+
+# this is the path to the static folder where css, js and images are stored
+STATIC_DIR = BASE_DIR / '/home/speebndt/quiz_frontend/static/'
+
+STATIC_URL = 'static/'
+STATIC_ROOT = '/home/speebndt/quiz.speedspro.us/static/'
+
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
